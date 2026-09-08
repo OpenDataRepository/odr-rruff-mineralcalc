d033991c9a57ac1a90009bb0a75d380cced03d49
nkromero <nkromero@asu.edu>
2026-09-08 14:22:51 -0700

    Bump WordPress plugin version to 1.2.0

--- wordpress-plugin/odr-rruff-mineralcalc/odr-rruff-mineralcalc.php
+++ wordpress-plugin/odr-rruff-mineralcalc/odr-rruff-mineralcalc.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: ODR RRUFF Mineral Calculator
  * Description: A plugin that gives a way to break down a mineral's valence formula into its elemental mass percentages, formula mass, and net charge
- * Version: 1.1.0
+ * Version: 1.2.0
  */
 
 if (!defined('ABSPATH')) {
